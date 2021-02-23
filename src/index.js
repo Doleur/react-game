9abddc925bc0b8a9457fb27839476c537cdf53e9
Doleur <voleur16@gmail.com>
2021-02-23 21:17:48 +0300

    init: start react-game

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./assets/components/app.component";
+ReactDOM.render( < App / > , document.querySelector("#root"));
